0ce4b6bf3f194a5e4bc31b4267a45a1d2bf06b0d
Gary Lu <madcated.lu@gmail.com>
2020-10-12 20:53:26 -0700

    Add CRUD methods of JobPosting

--- src/app/models/job_posting.py
+++ src/app/models/job_posting.py
@@ -1,3 +1,5 @@
+import uuid
+
 from app.db_operator import db
 
 
@@ -19,3 +21,35 @@ class JobPosting(db.Model):
     @classmethod
     def get(cls, job_posting_id):
         return cls.query.get(job_posting_id)
+
+    @classmethod
+    def create(cls, url, **kwargs):
+        job_posting_id = uuid.uuid4()
+        job_posting = cls(id=job_posting_id, url=url, **kwargs)
+        db.session.add(job_posting)
+        db.session.commit()
+        return job_posting
+
+    @classmethod
+    def update(cls, job_posting_id, **kwargs):
+        job_posting = db.session.query(cls).get(job_posting_id)
+
+        if not job_posting:
+            raise ValueError(u'job posting with id %s does not exist', job_posting_id)
+
+        for k, v in kwargs.items():
+            setattr(job_posting, k, v)
+
+        db.session.commit()
+        return job_posting
+
+    @classmethod
+    def delete(cls, job_posting_id):
+        job_posting = db.session.query(cls).get(job_posting_id)
+
+        if not job_posting:
+            raise ValueError(u'job posting with id %s does not exist', job_posting_id)
+
+        db.session.delete(job_posting)
+        db.session.commit()
+        return job_posting
--- src/main.py
+++ src/main.py
@@ -13,5 +13,10 @@ db.init_app(app)
 # TODO: kill these prototype code
 app.app_context().push()
 # print(JobPosting.query.all())
-jp = JobPosting.get("123")
-print(jp.url)
+# jp = JobPosting.get("123")
+# print(jp.url)
+
+# from datetime import datetime
+# jp = JobPosting.create(url="http://test.com", title="test create", posted_datetime=datetime.now())
+# jp = JobPosting.update(job_posting_id="123", posted_datetime=datetime.now())
+# jp = JobPosting.delete(job_posting_id="a1822c03-a321-4f50-910b-5d570c694a98")
