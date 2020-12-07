e6a1f6d345d7eb25bf3def1f01e983ed4962d9b6
Gary Lu <madcated.lu@gmail.com>
2020-12-06 18:03:19 -0800

    Update prototype

--- prototype/parse.py
+++ prototype/parse.py
@@ -1,10 +1,17 @@
+from urllib.parse import urlparse, urlunparse
+
 from bs4 import BeautifulSoup
+
+
+def remove_queries(url):
+    uu = urlparse(url)
+    return urlunparse(uu._replace(query=''))
 
 
 def parse_search_results(html_path):
     html_file = open(html_path, "r", encoding='utf-8')
     soup = BeautifulSoup(html_file.read(), 'html.parser')
-    result = [x['href'] for x in soup.find_all('a', class_='result-card__full-card-link')]
+    result = [remove_queries(x['href']) for x in soup.find_all('a', class_='result-card__full-card-link')]
     print(result)
 
 
@@ -17,5 +24,7 @@ def parse_single_posting(html_path):
     return job_title, job_description
 
 
-# parse_search_results("search_results.html")
-print(parse_single_posting("single_posting.html"))
+if __name__ == '__main__':
+
+    parse_search_results("search_results.html")
+    # print(parse_single_posting("single_posting.html"))
