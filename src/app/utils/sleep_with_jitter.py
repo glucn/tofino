17e8a133246d8d52d0546073da50be4758fa4502
Gary Lu <madcated.lu@gmail.com>
2020-12-28 10:55:37 -0800

    Jitter with larger range

--- src/app/utils/sleep_with_jitter.py
+++ src/app/utils/sleep_with_jitter.py
@@ -4,5 +4,5 @@ import time
 
 def sleep_with_jitter(base_sleep_seconds: int):
     jitter_range = int(base_sleep_seconds / 5)
-    sleep_seconds = base_sleep_seconds - int(jitter_range / 2) + random.randrange(0, jitter_range)
+    sleep_seconds = base_sleep_seconds + random.randrange(-jitter_range, jitter_range)
     time.sleep(sleep_seconds)
