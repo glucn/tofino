e942d57c161852db0b80e7d86c0ca9943bd213cd
Gary Lu <madcated.lu@gmail.com>
2021-01-02 22:43:30 -0800

    Use EntityRuler instead of PhraseMatcher

--- prototype/nlp.py
+++ prototype/nlp.py
@@ -1,48 +1,44 @@
 import json
+import sys
 
 import requests
 import spacy
-from spacy.matcher.phrasematcher import PhraseMatcher
-from spacy.tokens.doc import Doc
-from spacy.tokens.span import Span
+from spacy.pipeline import EntityRuler
 
-nlp = spacy.load('en_core_web_md')
 
-job_id = 'd399a130-57d1-4535-aa59-27adfbbe3b36'
-url = f'http://52.41.240.242/api/v1/jobPosting/{job_id}'
-
-jd = ''
-
-with requests.get(url, stream=True, allow_redirects=True) as response:
-    if response.status_code != 200:
-        raise Exception
-    job_posting = response.content
-    jd = json.loads(job_posting)['job_description']
-
-print(jd)
-
-skill_list = ['SQL', 'SAP', 'SAS', 'Python', 'ERP', 'BI', 'Microsoft Office', 'Dynamics', 'Excel',
+skill_list = ['SQL', 'SAP', 'SAS', 'Python', 'ERP', 'BI', 'MS Office', 'Microsoft Office', 'Dynamics', 'Excel', 'Word',
+              'PowerPoint', 'Workday',
               'Tableau', 'Java', 'Mongo', 'AWS', 'VBA', 'Oracle', 'SPSS', 'Javascript', 'Visio', 'Access', 'Git',
-              'Github', 'R', 'Salesforce', 'prioritization']
-patterns = list(nlp.pipe(skill_list))
-matcher = PhraseMatcher(nlp.vocab)
-matcher.add('SKILL', None, *patterns)
+              'Github', 'R', 'salesforce', 'Esuite', 'COGNOS',
+              'prioritization', 'communication', 'presentation',
+              ]
 
 
-def match_skill(doc: Doc):
-    matches = matcher(doc)
-    for _, start, end in matches:
-        # Generate Span representing the entity & set label
-        entity = Span(doc, start, end, label='SKILL')
-        # Overwrite doc.ents and add entity – be careful not to replace!
-        doc.ents = list(doc.ents) + [entity]
-
-    return doc
+def get_job_description(job_id):
+    url = f'http://tofin-Appli-9CZQN0QUMIV7-2117560493.us-west-2.elb.amazonaws.com/api/v1/jobPosting/{job_id}'
+    with requests.get(url, stream=True, allow_redirects=True) as response:
+        if response.status_code != 200:
+            raise Exception
+        job_posting = response.content
+        return json.loads(job_posting)['job_description']
 
 
-nlp.add_pipe(match_skill)
+if __name__ == '__main__':
+    job_id = sys.argv[1]
+    jd = get_job_description(job_id)
 
-with nlp.disable_pipes('tagger', 'parser', 'ner'):
-    doc = nlp(jd)
-    for ent in doc.ents:
-        print(ent.text, ent.label_)
+    print(jd)
+    print()
+
+    nlp = spacy.load('en_core_web_md')
+
+    ruler = EntityRuler(nlp, phrase_matcher_attr="LOWER")  # add `overwrite_ents=True` if after ner
+    patterns = [{"label": "SKILL", "pattern": skill} for skill in skill_list]
+    ruler.add_patterns(patterns)
+
+    nlp.add_pipe(ruler, before='ner')
+
+    with nlp.disable_pipes('tagger', 'parser'):
+        doc = nlp(jd)
+        for ent in doc.ents:
+            print(ent.text, ent.label_)
