9cab2ff74b1682d3487bc3c061fd79ee7d0fbf60
Gary Lu <madcated.lu@gmail.com>
2022-03-13 16:57:24 -0700

    Use DB in public subnet

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # AWS
 AWS_REGION = "us-west-2"
 
-MYSQL_HOST = "tmwsfdnrcwbmp4.ca9x6xep5ulo.us-west-2.rds.amazonaws.com"
+MYSQL_HOST = "dmlyvv2vevihuk.ca9x6xep5ulo.us-west-2.rds.amazonaws.com"
 MYSQL_DB_NAME = "TOFINO_DB"
 MYSQL_SECRET_NAME = "MySQL_application"
 
